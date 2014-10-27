ee5dd362b28e652cea9ce22ea1c55b4a0b6a7904
Willem van der Jagt <wkjagt@gmail.com>
2014-10-27 08:51:15 -0400

    implement fire method on command

--- src/Searchable/Command/IndexAll.php
+++ src/Searchable/Command/IndexAll.php
@@ -1,16 +1,34 @@
 <?php namespace Searchable\Command;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class IndexAll extends Command
 {
     /**
      * {@inheritdoc}
      */
-    protected $name = 'command.searchable.indexall';
+    protected $name = 'searchable:indexall';
 
     /**
      * {@inheritdoc}
      */
     protected $description = 'Builds a complete Searchable index for an Eloquent model';
+
+    protected function getArguments()
+    {
+        return [['modelclass', InputArgument::REQUIRED, 'The model class to index']];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fire()
+    {
+        $modelClassArg = $this->argument('modelclass');
+
+        $modelClass = str_replace('/', '\\', $modelClassArg);
+
+        $this->laravel->make($modelClass)->indexAll();
+    }
 } 
