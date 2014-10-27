5278158ca27870798541378ab9cb1e9bb9df7b9c
Willem van der Jagt <wkjagt@gmail.com>
2014-10-26 21:16:17 -0400

    give indexall command a description

--- src/Searchable/Command/IndexAll.php
+++ src/Searchable/Command/IndexAll.php
@@ -8,4 +8,9 @@ class IndexAll extends Command
      * {@inheritdoc}
      */
     protected $name = 'command.searchable.indexall';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = 'Builds a complete Searchable index for an Eloquent model';
 } 
