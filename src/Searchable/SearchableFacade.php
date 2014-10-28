5a2df06db026a8787810a9c79064dd181cd37b4c
Willem van der Jagt <wkjagt@gmail.com>
2014-10-27 22:11:29 -0400

    document SearchableFacade

--- src/Searchable/SearchableFacade.php
+++ src/Searchable/SearchableFacade.php
@@ -4,8 +4,19 @@ namespace Searchable;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Facade definition for Searchable
+ *
+ * Class SearchableFacade
+ * @package Searchable
+ */
 class SearchableFacade extends Facade
 {
+    /**
+     * Get facade accessor.
+     *
+     * @return string the name of the service to get from the IoC container
+     */
     protected static function getFacadeAccessor()
     {
         return 'searchable';
