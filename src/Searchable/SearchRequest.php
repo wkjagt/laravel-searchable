6895af1fc3fa182d606955a46df3b603a9401e7f
Willem van der Jagt <wkjagt@gmail.com>
2014-10-27 22:14:38 -0400

    document SearchRequest class

--- src/Searchable/SearchRequest.php
+++ src/Searchable/SearchRequest.php
@@ -4,13 +4,31 @@ namespace Searchable;
 
 use Illuminate\Support\Facades\App;
 
+/**
+ * Representation of a search request
+ *
+ * Class SearchRequest
+ * @package Searchable
+ */
 class SearchRequest
 {
+    /**
+     * Add the fully qualified class name of a model to the list of
+     * models to search.
+     *
+     * @param string $model
+     */
     public function addModel($model)
     {
         $this->models[] = $model;
     }
 
+    /**
+     * The query to use to search on the specified models.
+     *
+     * @param array $query
+     * @return array an array of search results
+     */
     public function withQuery(array $query)
     {
         return App::make('searchable.engine')->search($this->models, $query);
