0ce452638a895d8f31e2638453c82df76544d933
Willem van der Jagt <wkjagt@gmail.com>
2014-10-27 22:08:44 -0400

    document Searchable class

--- src/Searchable/Searchable.php
+++ src/Searchable/Searchable.php
@@ -4,8 +4,19 @@ namespace Searchable;
 
 use InvalidArgumentException;
 
+/**
+ * The access point to most search functionality.
+ *
+ * @package Searchable
+ */
 class Searchable
 {
+    /**
+     * Search in multiple models.
+     *
+     * @param array $models an array of model fully qualified model class names
+     * @return SearchRequest
+     */
     public function search(array $models)
     {
         $request = new SearchRequest();
